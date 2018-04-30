2361641f7061e27568232a0f7795ad0044d0cd6a
qhanam <qhanam@gmail.com>
2018-04-29 20:15:34 -0500

    add statement and expression handlers

--- utilities/esseq.js
+++ utilities/esseq.js
@@ -44,7 +44,7 @@
 		},
 
 		FunctionDeclaration: function(node) {
-			let seq = expressionGenerator.FunctionDeclaration(node);
+			let seq = expressionGenerator.FunctionExpression(node);
 			return seq;
 		},
 
@@ -130,6 +130,71 @@
 			join(seq, expressionGenerator[right.type](right));
 			seq.push(')');
 			join(seq, statementGenerator[body.type](body));
+			return seq;
+		},
+
+		IfStatement: function(node) {
+			let seq = ['if','('],
+				test = node.test,
+				consequent = node.consequent,
+				alternate = node.alternate;
+			join(seq, expressionGenerator[test.type](test));
+			seq.push(')');
+			join(seq, statementGenerator[consequent.type](consequent));
+			if(alternate !== null) {
+				seq.push('else');
+				join(seq, statementGenerator[alternate.type](alternate));
+			}
+			return seq;
+		},
+
+		DebuggerStatement: function(node) {
+			return ['debugger',';'];
+		},
+
+		ReturnStatement: function(node) {
+			let seq = ['return'],
+				argument = node.argument;
+			console.log(argument.type);
+			if(argument !== null) join(seq, expressionGenerator[argument.type](argument));
+			seq.push(';');
+			return seq;
+		},
+
+		SwitchStatement: function(node) {
+			let seq = ['switch','('],
+				discriminant = node.discriminant,
+				cases = node.cases;
+			join(seq, expressionGenerator[discriminant.type](discriminant));
+			seq.push(')');
+			seq.push('{');
+			for(let i = 0; i < cases.length; i++) {
+				let cas = cases[i];
+				join(seq, expressionGenerator[cas.type](cas));
+			}
+			seq.push('}');
+			return seq;
+		},
+
+		ThrowStatement: function(node) {
+			let seq = ['throw'],
+				argument = node.argument;
+			join(seq, expressionGenerator[argument.type](argument));
+			seq.push(';');
+			return seq;
+		},
+
+		TryStatement: function(node) {
+			let seq = ['try'],
+				block = node.block,
+				handler = node.handler,
+				finalizer = node.finalizer;
+			join(seq, statementGenerator[block.type](block));
+			join(seq, expressionGenerator[handler.type](handler));
+			if(finalizer !== null) {
+				seq.push('finally');
+				join(seq, statementGenerator[finalizer.type](finalizer));
+			}
 			return seq;
 		}
 
@@ -236,7 +301,7 @@
 			return seq;
 		},
 
-		FunctionDeclaration: function(node) {
+		FunctionExpression: function(node) {
 			let seq = ['function'],
 				id = node.id,
 				params = node.params,
@@ -250,6 +315,129 @@
 			}
 			seq.push(')');
 			join(seq, statementGenerator[body.type](body));
+			return seq;
+		},
+
+		BinaryExpression: function(node) {
+			let seq = [],
+				operator = node.operator,
+				left = node.left,
+				right = node.right;
+			join(seq, expressionGenerator[left.type](left));
+			seq.push(operator);
+			join(seq, expressionGenerator[right.type](right));
+			return seq;
+		},
+
+		AssignmentExpression: function(node) {
+			return expressionGenerator.BinaryExpression(node);
+		},
+
+		LogicalExpression: function(node) {
+			return expressionGenerator.BinaryExpression(node);
+		},
+
+		ThisExpression: function(node) {
+			return ['this'];
+		},
+
+		NewExpression: function(node) {
+			let seq = ['new'],
+				callee = node.callee,
+				args = node.arguments;
+			join(seq, expressionGenerator[callee.type](callee));
+			seq.push('(');
+			for(let i = 0; i < args.length; i++) {
+				let arg = args[i];
+				join(seq, expressionGenerator[arg.type](arg));
+				if(i !== args.length - 1) seq.push(',');
+			}
+			seq.push(')');
+			return seq;
+		},
+
+		ObjectExpression: function(node) {
+			let seq = ['{'],
+				properties = node.properties;
+			for(let i = 0; i < properties.length; i++) {
+				let property = properties[i];
+				join(seq, expressionGenerator[property.type](property));
+				if(i !== properties.length - 1) seq.push(',');
+			}
+			seq.push('}');
+			return seq;
+		},
+
+		Property: function(node) {
+			let seq = [],
+				key = node.key,
+				value = node.value,
+				computed = node.computed,
+				kind = node.kind,
+				method = node.method,
+				shorthand = node.shorthand;
+			if(computed) {
+				seq.push('[');
+				join(seq, expressionGenerator[key.type](key));
+				seq.push(']');
+			}
+			else {
+				join(seq, expressionGenerator[key.type](key));
+			}
+			seq.push(':');
+			join(seq, expressionGenerator[value.type](value));
+			return seq;
+		},
+
+		SwitchCase: function(node) {
+			let seq = [],
+				test = node.test,
+				consequent = node.consequent;
+			if(test === null) {
+				seq.push('default');
+			}
+			else {
+				seq.push('case');
+				join(seq, expressionGenerator[test.type](test));
+			}
+			seq.push(':');
+			for(let i = 0; i < consequent.length; i++) {
+				let stmt = consequent[i];
+				join(seq, statementGenerator[stmt.type](stmt));
+			}
+			return seq;
+		},
+
+		CatchClause: function(node) {
+			let seq = ['catch','('],
+				param = node.param,
+				body = node.body;
+			join(seq, expressionGenerator[param.type](param));
+			seq.push(')');
+			join(seq, statementGenerator[body.type](body));
+			return seq;
+		},
+
+		UnaryExpression: function(node) {
+			let seq = [],
+				operator = node.operator,
+				argument = node.argument,
+				prefix = node.prefix;
+			if(prefix) seq.push(operator);
+			join(seq, expressionGenerator[argument.type](argument));
+			if(!prefix) seq.push(operator);
+			return seq;
+		},
+
+		UpdateExpression: function(node) {
+			return expressionGenerator.UnaryExpression(node);
+		},
+
+		YieldExpression: function(node) {
+			let seq = ['yield'],
+				argument = node.argument,
+				delegate = node.delegate;
+			if(argument !== null) join(seq, expressionGenerator[argument.type](argument));
 			return seq;
 		}
 
