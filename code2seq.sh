4ca4b2fc903c0cc4196c7e9c55f4a631fed1642b
qhanam <qhanam@gmail.com>
2018-05-02 18:45:41 -0500

    add two more sequence generation tests

--- code2seq.sh
+++ code2seq.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #node --max-old-space-size=24576 code2seq.js --code ../flow-mining/output/results.json --topn ./output/topn --seq ./output/seq --vocab ./output/vocab --buckets 10
-node --max-old-space-size=24576 code2seq.js --code ../flow-mining/output/tmp.json --topn ./output/topn --seq ./output/seq --vocab ./output/vocab --buckets 10
+#node --max-old-space-size=24576 code2seq.js --code ../flow-mining/output/tmp.json --topn ./output/topn --seq ./output/seq --vocab ./output/vocab --buckets 10
+node --max-old-space-size=24576 code2seq.js --code ./tests/block.json --topn ./output/topn --seq ./output/seq --vocab ./output/vocab --buckets 10
